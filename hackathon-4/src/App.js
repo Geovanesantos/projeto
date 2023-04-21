9714caf2a43a24e63e552fee381f0fe90bff9926
unknown <GEOVANE.DOSSANTOS@lince.com.br>
2023-04-21 09:16:09 -0300

    teste

---
+++ hackathon-4/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import  HelpDesk  from '../src/helpDesk/page-1/pages/helpDesk';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <HelpDesk/>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
